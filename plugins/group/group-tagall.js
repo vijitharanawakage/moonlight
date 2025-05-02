be65c9f1541b46b40ff7642fcc225fb6cf2c2964
Yugesh Singh <yugeshsingh27@gmail.com>
2025-05-02 19:00:53 +0530

    Update group-tagall.js

--- plugins/group/group-tagall.js
+++ plugins/group/group-tagall.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
   m.reply(
     `▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` +
       users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` +
-      '\n└──✪ GURU ┃ ᴮᴼᵀ ✪──',
+      '\nShizo Moonlight',
     null,
     {
       mentions: users,
